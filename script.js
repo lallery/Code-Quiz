0e55baf36cb354f0cbcea5c53d8cf005e0bbd7c0
lallery <64818633+lallery@users.noreply.github.com>
2020-06-03 21:25:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+var statusToggle = document.querySelector("#status-toggle");
+var playButton = document.querySelector("#play");
+var pauseButton = document.querySelector("#pause");
+var stopButton = document.querySelector("#stop");
+var minutesDisplay = document.querySelector("#minutes");
+var secondsDisplay = document.querySelector("#seconds");
+var workMinutesInput = document.querySelector("#work-minutes");
+var restMinutesInput = document.querySelector("#rest-minutes"
